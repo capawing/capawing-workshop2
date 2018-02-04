7ac8dcdc9c9ac9472a4e766dee91d4a1ca2172da
capawing <35475195+capawing@users.noreply.github.com>
2018-02-04 04:07:08 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Workshop2\capawing</title>
+	<title>WORKSHOP2</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src= "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </head>
 <body>
-SIR AKO RA I SUNOD ANG PIC
-</body>
-</html>
+<div class = "mainBase">
+	<div class = "menuItem">
+			<a style="float:left"; class = "linker" href="php/About.php" id = "about">HOME</a>
+			<a style="float:left"; class = "linker" href="php/cabling.php" id = "profile">TYPES OF CABLING</a>
+			<a style="float:left"; class = "linker" href="php/fiber.php" id = "contact" href="">FIBER OPTIC CONNECTORS</a>
+			<a style="float:left"; class = "linker" href="php/packet.php" id = "signup" href="">CISCO PACKET TRACER</a>
+		</div>
+	<img src="img/24.jpg" width="100%" height="100%">
+	<!--Login Base-->
